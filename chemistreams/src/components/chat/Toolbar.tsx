6d88e3685d8d0af4fe2b0ed71835f173b6941ab6
Robby Dharni <robbydharni@Robbys-MacBook-Pro.local>
2025-04-08 22:17:34 -0700

    lucide icons

--- chemistreams/src/components/chat/Toolbar.tsx
+++ chemistreams/src/components/chat/Toolbar.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { ICON_EXIT, ICON_PAINT, ICON_PROFILE, ICON_TRASH, LOGO, SQUARE_IMAGE_SIZE } from "@/lib/constants/client"
+import { UserPen, PaintBucket, Trash2, LogOut } from "lucide-react"
+import { LOGO, SQUARE_IMAGE_SIZE } from "@/lib/constants/client"
 import { ToolbarButtonProps } from "@/lib/types/props"
 import ToolbarButton from "./Toolbar/ToolbarButton"
 import Image from "next/image"
@@ -8,36 +9,32 @@ import Image from "next/image"
 export default function Toolbar() {
     const buttons: ToolbarButtonProps[] = [
         {
-            icon: ICON_PROFILE,
-            alt: "Profile",
+            Icon: UserPen,
             callback: () => {}
         },
         {
-            icon: ICON_PAINT,
-            alt: "Theme",
+            Icon: PaintBucket,
             callback: () => {}
         },
         {
-            icon: ICON_TRASH,
-            alt: "Delete",
+            Icon: Trash2,
             callback: () => {}
         },
         {
-            icon: ICON_EXIT,
-            alt: "Logout",
+            Icon: LogOut,
             callback: () => {}
         }
     ]
 
     return (
         <section className="bg-black border-r border-dark-grey md:h-full md:w-[7.5%] md:p-2">
-            <div className="w-full h-[70%] flex flex-col justify-between items-center">
+            <div className="w-full h-[65%] flex flex-col justify-between items-center">
                 <Image src={LOGO} alt="Logo" width={SQUARE_IMAGE_SIZE} height={SQUARE_IMAGE_SIZE} className="md:w-full md:aspect-square p-3" />
 
                 <ul className="md:w-full md:h-auto flex flex-col items-center space-y-8">
                     {
                         buttons.map((button: ToolbarButtonProps, index: number) => {
-                            return <ToolbarButton key={`TOOLBAR_BUTTON_${index}`} icon={button.icon} alt={button.alt} callback={button.callback} />
+                            return <ToolbarButton key={`TOOLBAR_BUTTON_${index}`} Icon={button.Icon} callback={button.callback} />
                         })
                     }
                 </ul>
--- chemistreams/src/components/chat/Toolbar/ToolbarButton.tsx
+++ chemistreams/src/components/chat/Toolbar/ToolbarButton.tsx
@@ -1,13 +1,11 @@
-import { SQUARE_IMAGE_SIZE } from "@/lib/constants/client"
 import { ToolbarButtonProps } from "@/lib/types/props"
-import Image from "next/image"
 
-export default function ToolbarButton({ icon, alt, callback } : ToolbarButtonProps) {
+export default function ToolbarButton({ Icon, callback } : ToolbarButtonProps) {
 
     return (
         <li className="md:w-2/5 md:aspect-square">
             <button onClick={() => { callback() }} className="md:w-full md:aspect-square">
-                <Image src={icon} alt={alt} width={SQUARE_IMAGE_SIZE} height={SQUARE_IMAGE_SIZE} className="md:w-full md:aspect-square" />
+                <Icon className="md:text-light-grey hover:text-green hover:cursor-pointer md:w-full md:aspect-square" />
             </button>
         </li>
     )
