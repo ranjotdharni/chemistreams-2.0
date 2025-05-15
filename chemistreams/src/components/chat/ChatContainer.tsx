465c86ce8ec38218a50b0c836c8bcf0d513d5bee
Robby Dharni <robbydharni@Robbys-MacBook-Pro.local>
2025-05-14 17:18:23 -0700

    delete chats function complete

--- chemistreams/src/components/chat/ChatContainer.tsx
+++ chemistreams/src/components/chat/ChatContainer.tsx
@@ -1,10 +1,10 @@
 "use client"
 
 import { ChatMetaData, DirectChatMetaData, GenericError, GroupChatMetaData, GroupMember } from "@/lib/types/client"
+import { DB_GROUPS, DB_MESSAGES, DB_METADATA, DB_USERS } from "@/lib/constants/routes"
+import { DataSnapshot, get, onDisconnect, ref, set, update } from "firebase/database"
 import { MouseEvent, useCallback, useContext, useMemo, useState } from "react"
 import { useDatabaseErrorHandler } from "@/lib/hooks/useDatabaseErrorHandler"
-import { DataSnapshot, get, onDisconnect, ref, set } from "firebase/database"
-import { DB_GROUPS, DB_METADATA, DB_USERS } from "@/lib/constants/routes"
 import { LogOut, PaintBucket, Trash2, UserPen } from "lucide-react"
 import { InterfaceContext } from "@/lib/context/InterfaceContext"
 import { AuthContext } from "@/lib/context/AuthContext"
@@ -32,6 +32,7 @@ export default function ChatContainer() {
     const [chatList, setChatList] = useState<ChatMetaData[]>([])
     const [statusValueErrorCallback, setStatusValueErrorCallback] = useDatabaseErrorHandler("CHATCONTAINER_STATUS_CHANGE_ERROR")
     const [chatAddedErrorCallback, setChatAddErrorCallback] = useDatabaseErrorHandler("CHATCONTAINER_CHAT_ADD_ERROR")
+    const [chatRemovedErrorCallback, setChatRemovedErrorCallback] = useDatabaseErrorHandler("CHATCONTAINER_CHAT_REMOVE_ERROR")
 
     const buttons: ToolbarButtonProps[] = [
         {
@@ -46,7 +47,8 @@ export default function ChatContainer() {
         },
         {
             Icon: Trash2,
-            callback: () => {}
+            hoverColor: "red",
+            callback: handleDelete
         },
         {
             Icon: LogOut,
@@ -81,6 +83,52 @@ export default function ChatContainer() {
         setShowProfileEditor(value => {
             return !value
         })
+    }
+
+    async function handleDelete(event: MouseEvent<HTMLButtonElement>) {
+        event.preventDefault()
+
+        if (!user || !currentChat)
+            return
+
+        const deletingChatId: string = currentChat.id
+
+        const metadataSnapshot = await get(ref(rt, `${DB_METADATA}/${deletingChatId}`))
+        const metadata = metadataSnapshot.val()
+
+        if (!metadata || metadata.creator !== user.uid) {
+            UIControl.setText("Insufficient privileges.", "red")
+            return
+        }
+
+        let updates: Record<string, null> = {
+            [`${DB_METADATA}/${deletingChatId}`]: null,
+            [`${DB_GROUPS}/${deletingChatId}`]: null,
+            [`${DB_MESSAGES}/${deletingChatId}`]: null
+        }
+
+        if ((currentChat as GroupChatMetaData).isGroup) {
+            // group chat
+            (currentChat as GroupChatMetaData).members.forEach(member => {
+                updates[`${DB_USERS}/${member.id}/chats/${deletingChatId}`] = null
+            })
+        }
+        else {
+            // direct chat
+            updates[`${DB_USERS}/${(currentChat as DirectChatMetaData).to}/chats/${deletingChatId}`] = null    // delete recipient chat reference
+            updates[`${DB_USERS}/${currentChat.creator}/chats/${deletingChatId}`] = null                       // delete creator chat reference
+        }
+
+        try {
+            await update(ref(rt), updates)
+        }
+        catch (error) {
+            UIControl.setText("FATAL ERROR: Internal Server Error 500", "red")
+            return
+        }
+
+        setCurrentChat(undefined)
+        UIControl.setText("Chat deleted.", "green")
     }
 
     async function handleLogout(event: MouseEvent<HTMLButtonElement>) {
@@ -165,6 +213,36 @@ export default function ChatContainer() {
         }
     }, [user])
 
+    const handleDeletedChat = useCallback(async (snapshot: DataSnapshot) => {
+        const deletedChatId = snapshot.key
+
+        if (!deletedChatId)
+            return
+
+        setChatList(list => {
+            let updatedList = [...list]
+            let removeIndex = updatedList.findIndex(c => c.id === deletedChatId)
+
+            setCurrentChat(chat => {
+                if (!chat)
+                    return chat
+
+                if (chat.id === deletedChatId)
+                    return undefined
+
+                return chat
+            })
+
+            if (removeIndex !== -1) {
+                updatedList.splice(removeIndex, 1)
+                return updatedList
+            }
+            else {
+                return list
+            }
+        })
+    }, [user])
+
     const handleStatusChange = useCallback(async (snapshot: DataSnapshot) => {
         if (snapshot.val() === null || snapshot.val() === undefined)
             return
@@ -184,9 +262,13 @@ export default function ChatContainer() {
             added: {
                 callback: handleNewChat,
                 errorCallback: chatAddedErrorCallback
+            },
+            removed: {
+                callback: handleDeletedChat,
+                errorCallback: chatRemovedErrorCallback
             }
         }
-    }, [handleNewChat, chatAddedErrorCallback])
+    }, [user])
 
     const statusListenerConfig: UseListenerConfig = useMemo(() => {
         return {
@@ -195,7 +277,7 @@ export default function ChatContainer() {
                 errorCallback: statusValueErrorCallback
             }
         }
-    }, [user.uid])
+    }, [user])
 
     useListener(chatListReference, chatListListenerConfig)
     useListener(connectionReference, statusListenerConfig, undefined, async () => { await set(statusReference, false) })
