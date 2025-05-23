9b1aeb439e7eaaeab7344b58d160e3c6454304b4
Robby Dharni <robbydharni@Robbys-MacBook-Pro.local>
2025-05-23 14:38:01 -0700

    landing page complete

---
+++ chemistreams/src/components/landing/Footer.tsx
@@ -0,0 +1,35 @@
+
+export default function Footer() {
+    const footerItems = [
+        {
+            link: "https://github.com/ranjotdharni",
+            text: "GitHub"
+        },
+        {
+            link: "https://www.linkedin.com/in/ranjot-dharni-717580269/",
+            text: "LinkedIn"
+        },
+        {
+            link: "https://icons8.com",
+            text: "Icons8"
+        },
+        {
+            link: "https://app.spline.design/community/file/8cfb6748-f3dd-44dd-89fb-f46c7ab4186e",
+            text: "Spline"
+        },
+        {
+            link: "https://www.reddit.com/r/webdev/comments/oqvsrj/chatapp_react_portfolio_project_live_link_source/",
+            text: "Design"
+        }
+    ]
+
+    return (
+        <footer className="fixed top-[90vh] left-0 w-full h-[10vh] bg-black border-t border-dark-grey flex flex-row justify-center items-center space-x-8">
+            {
+                footerItems.map((item, index) => {
+                    return <a key={`LANDING_FOOTER_ITEM_${index}`} href={item.link} target="_blank" className="hover:cursor-pointer hover:text-green text-light-grey underline font-lato">{item.text}</a>
+                })
+            }
+        </footer>
+    )
+}
---
+++ chemistreams/src/components/utils/PublicNavbar.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { PAGE_LANDING, PAGE_LOGIN, PAGE_SIGNUP, PUBLIC_ROUTES } from "@/lib/constants/routes"
+import { usePathname } from "next/navigation"
+import AnimatedLogo from "./AnimatedLogo"
+
+export default function PublicNavbar() {
+    const pathname = usePathname()
+
+    if (!PUBLIC_ROUTES.includes(pathname))
+        return <></>
+
+    return (
+        <nav className="fixed top-0 left-0 bg-black border-b border-dark-grey fixed pr-30 w-full h-[10vh] p-2 flex flex-row justify-between items-center">
+            <a href={PAGE_LANDING} className="hover:cursor-pointer h-full flex flex-row items-center space-x-2 p-1">
+                <div className="h-5 w-15">
+                    <AnimatedLogo />
+                </div>
+                <p className="text-green font-lato text-4xl">ChemiStreams</p>
+            </a>
+
+            <div className="text-lg space-x-15">
+                <a href={PAGE_LOGIN} className="font-jbm hover:cursor-pointer" style={{color: PAGE_LOGIN === pathname ? "var(--color-green)" : "var(--color-light-grey)"}}>Log In</a>
+                <a href={PAGE_SIGNUP} className="font-jbm hover:cursor-pointer" style={{color: PAGE_SIGNUP === pathname ? "var(--color-green)" : "var(--color-light-grey)"}}>Sign Up</a>
+                <a href="https://github.com/ranjotdharni/chemistreams-2.0" target="_blank" className="font-jbm hover:cursor-pointer text-light-grey">Usage</a>
+                <a href="https://github.com/ranjotdharni/chemistreams-2.0" target="_blank" className="font-jbm hover:cursor-pointer text-light-grey">About</a>
+            </div>
+        </nav>
+    )
+}
